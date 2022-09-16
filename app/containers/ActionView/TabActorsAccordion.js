4d6e0d86ea5fb63e17d91482292e766bb45055e3
tmfrnz <timo@dumpark.com>
2022-09-16 17:02:59 +0200

    cleanup #133

--- app/containers/ActionView/TabActorsAccordion.js
+++ app/containers/ActionView/TabActorsAccordion.js
@@ -93,7 +93,7 @@ export function TabActorsAccordion({
             }}
           />
         )}
-        {(hasChildTargets || actorsByType) && (
+        {hasChildTargets && (
           <Accordion
             activeIndex={actives}
             onActive={(newActive) => setActive(newActive)}
