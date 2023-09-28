23d806472c01728fe6d2891445abf6dc14f7b2a4
vbojilova <viki.bojilova@gmail.com>
2023-09-28 15:42:23 -0300

    adding print view to indicator list and page list - #174

--- app/containers/PageList/index.js
+++ app/containers/PageList/index.js
@@ -4,13 +4,14 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { Map, List, fromJS } from 'immutable';
+import { injectIntl, intlShape } from 'react-intl';
 
-import { loadEntitiesIfNeeded, updatePath } from 'containers/App/actions';
+import { loadEntitiesIfNeeded, updatePath, printView } from 'containers/App/actions';
 import {
   selectReady,
   selectIsUserMember,
@@ -21,87 +22,98 @@ import { ROUTES } from 'themes/config';
 
 import EntityList from 'containers/EntityList';
 
+import { PRINT_TYPES } from 'containers/App/constants';
+import { keydownHandlerPrint } from 'utils/print';
 import { CONFIG, DEPENDENCIES } from './constants';
 import { selectListPages } from './selectors';
 import messages from './messages';
 
-export class PageList extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  UNSAFE_componentWillMount() {
-    this.props.loadEntitiesIfNeeded();
-  }
+export function PageList({
+  onLoadEntitiesIfNeeded,
+  onSetPrintView,
+  dataReady,
+  handleNew,
+  isMember,
+  location,
+  entities,
+  allEntities,
+  intl,
+}) {
+  useEffect(() => {
+    if (!dataReady) onLoadEntitiesIfNeeded();
+  }, [dataReady]);
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    // reload entities if invalidated
-    if (!nextProps.dataReady) {
-      this.props.loadEntitiesIfNeeded();
-    }
-  }
-
-  render() {
-    const { intl } = this.context;
-    const {
-      dataReady,
-      isMember,
-      location,
-      allEntities,
-    } = this.props;
-    const headerOptions = {
-      supTitle: intl.formatMessage(messages.pageTitle),
-      actions: [],
+  const mySetPrintView = () => onSetPrintView({
+    printType: PRINT_TYPES.LIST,
+    printContentOptions: { pages: true },
+    printOrientation: 'portrait',
+    printSize: 'A4',
+  });
+  const keydownHandler = (e) => {
+    keydownHandlerPrint(e, mySetPrintView);
+  };
+  useEffect(() => {
+    document.addEventListener('keydown', keydownHandler);
+    return () => {
+      document.removeEventListener('keydown', keydownHandler);
     };
-    if (window.print) {
-      headerOptions.actions.push({
-        type: 'icon',
-        onClick: () => window.print(),
-        title: 'Print',
-        icon: 'print',
-      });
-    }
-    if (isMember) {
-      headerOptions.actions.push({
-        title: 'Create new',
-        onClick: () => this.props.handleNew(),
-        icon: 'add',
-        isMember,
-      });
-    }
-    return (
-      <div>
-        <Helmet
-          title={intl.formatMessage(messages.pageTitle)}
-          meta={[
-            { name: 'description', content: intl.formatMessage(messages.metaDescription) },
-          ]}
-        />
-        <EntityList
-          entities={this.props.entities && this.props.entities}
-          allEntityCount={allEntities && allEntities.size}
-          config={CONFIG}
-          headerOptions={headerOptions}
-          dataReady={dataReady}
-          entityTitle={{
-            single: intl.formatMessage(appMessages.entities.pages.single),
-            plural: intl.formatMessage(appMessages.entities.pages.plural),
-          }}
-          locationQuery={fromJS(location.query)}
-        />
-      </div>
-    );
+  });
+
+  const headerOptions = {
+    supTitle: intl.formatMessage(messages.pageTitle),
+    actions: [],
+  };
+  if (window.print) {
+    headerOptions.actions.push({
+      type: 'icon',
+      onClick: () => mySetPrintView(),
+      title: 'Print',
+      icon: 'print',
+    });
   }
+  if (isMember) {
+    headerOptions.actions.push({
+      title: 'Create new',
+      onClick: () => handleNew(),
+      icon: 'add',
+      isMember,
+    });
+  }
+  return (
+    <div>
+      <Helmet
+        title={intl.formatMessage(messages.pageTitle)}
+        meta={[
+          { name: 'description', content: intl.formatMessage(messages.metaDescription) },
+        ]}
+      />
+      <EntityList
+        entities={entities}
+        allEntityCount={allEntities && allEntities.size}
+        config={CONFIG}
+        headerOptions={headerOptions}
+        dataReady={dataReady}
+        entityTitle={{
+          single: intl.formatMessage(appMessages.entities.pages.single),
+          plural: intl.formatMessage(appMessages.entities.pages.plural),
+        }}
+        locationQuery={fromJS(location.query)}
+      />
+    </div>
+  );
 }
 
+
 PageList.propTypes = {
-  loadEntitiesIfNeeded: PropTypes.func,
+  onLoadEntitiesIfNeeded: PropTypes.func,
+  onSetPrintView: PropTypes.func,
   handleNew: PropTypes.func,
   dataReady: PropTypes.bool,
   entities: PropTypes.instanceOf(List).isRequired,
   allEntities: PropTypes.instanceOf(Map),
   location: PropTypes.object,
   isMember: PropTypes.bool,
-};
-
-PageList.contextTypes = {
-  intl: PropTypes.object.isRequired,
+  intl: intlShape.isRequired,
 };
 
 const mapStateToProps = (state, props) => ({
@@ -110,15 +122,19 @@ const mapStateToProps = (state, props) => ({
   isMember: selectIsUserMember(state),
   allEntities: selectPages(state),
 });
+
 function mapDispatchToProps(dispatch) {
   return {
-    loadEntitiesIfNeeded: () => {
+    onLoadEntitiesIfNeeded: () => {
       DEPENDENCIES.forEach((path) => dispatch(loadEntitiesIfNeeded(path)));
     },
     handleNew: () => {
       dispatch(updatePath(`${ROUTES.PAGES}${ROUTES.NEW}`, { replace: true }));
     },
+    onSetPrintView: (config) => {
+      dispatch(printView(config));
+    },
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PageList);
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(PageList));
