9c4ec336848635132e520419dacd96a8f4c13dd3
tmfrnz <timo@dumpark.com>
2024-11-11 20:43:07 +0100

    add note WIP #225

--- app/containers/NavSecondary/index.js
+++ app/containers/NavSecondary/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
 import { updatePath } from 'containers/App/actions';
 
 import PrintHide from 'components/styled/PrintHide';
@@ -34,6 +34,18 @@ const NavMain = styled(PrintHide)`
   position: relative;
 `;
 
+const Note = styled(Text)`
+  font-family: ${({ theme }) => theme.fonts.title};
+  font-size: ${({ theme }) => theme.text.medium.size};
+  line-height: ${({ theme }) => theme.text.medium.size};
+  border-bottom: 4px solid transparent;
+  padding: 5px 0.5em 5px;
+  opacity: 0.4;
+  @media print {
+    font-size: ${({ theme }) => theme.sizes.header.print.title};
+  }
+`;
+
 const StyledContainer = styled(Container)`
   padding-bottom: 0;
 `;
@@ -46,22 +58,27 @@ class NavSecondary extends React.PureComponent { // eslint-disable-line react/pr
         <StyledContainer>
           <Content>
             <NavMain>
-              <Box direction="row" align="end" fill>
-                {navItems && navItems.map((item, i) => (
-                  <LinkMain
-                    key={i}
-                    href={item.path}
-                    active={item.active}
-                    onClick={(evt) => {
-                      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-                      onPageLink(item.path);
-                    }}
-                  >
-                    <LinkTitle active={item.active}>
-                      {item.title}
-                    </LinkTitle>
-                  </LinkMain>
-                ))}
+              <Box direction="row" justify="between" align="end" fill>
+                <Box direction="row" align="end" fill>
+                  {navItems && navItems.map((item, i) => (
+                    <LinkMain
+                      key={i}
+                      href={item.path}
+                      active={item.active}
+                      onClick={(evt) => {
+                        if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+                        onPageLink(item.path);
+                      }}
+                    >
+                      <LinkTitle active={item.active}>
+                        {item.title}
+                      </LinkTitle>
+                    </LinkMain>
+                  ))}
+                </Box>
+                <Note>
+                  For authorised use only. Share with care.
+                </Note>
               </Box>
             </NavMain>
           </Content>
